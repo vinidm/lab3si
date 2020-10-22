8d5f9fa37cbeb43917683b92607b1176ecdd0302
Vini Del Manto <vinidmc@usp.br>
2020-10-22 14:39:21 -0300

    calculadora com memória

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Input } from "./components/Input";
 import { Display } from "./components/Display";
 import { ClearButton } from "./components/ClearButton";
 import { SignalButton } from "./components/SignalButton";
-
+import { MemoryButton } from "./components/MemoryButton";
 
 class App extends Component {
   constructor(props) {
@@ -14,7 +14,8 @@ class App extends Component {
     this.state = {
       previousOperand: '',
       currentOperand: '',
-      operation: ''
+      operation: '',
+      memory: ['','','','','','','','','']
     };
   }
 
@@ -58,12 +59,175 @@ class App extends Component {
     });
   };
 
+  memoryOperation = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = ['','','','','','','','',''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[0];
+    }
+    if (val == "M+"){
+      memnew[0] = parseFloat(memnew[0])+parseFloat(newoperand);
+    }
+    if (val == "MS"){
+      memnew = [newoperand,memnew[0],memnew[1],memnew[2],memnew[3],memnew[4],memnew[5],memnew[6],memnew[7]];
+      newoperand = ''
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory1 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[1],memnew[2],memnew[3],memnew[4],memnew[5],memnew[6],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[0];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory2 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[2],memnew[3],memnew[4],memnew[5],memnew[6],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[1];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory3 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[3],memnew[4],memnew[5],memnew[6],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[2];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory4 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[2],memnew[4],memnew[5],memnew[6],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[3];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory5 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[2],memnew[3],memnew[5],memnew[6],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[4];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory6 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[2],memnew[3],memnew[4],memnew[6],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[5];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory7 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[2],memnew[3],memnew[4],memnew[5],memnew[7],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[6];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory8 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[2],memnew[3],memnew[4],memnew[5],memnew[6],memnew[8],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[7];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
+  operationinMemory9 = val => {
+    var newoperand = this.state.currentOperand;
+    var memnew = this.state.memory
+    if (val == "MC"){
+      memnew = [memnew[0],memnew[1],memnew[2],memnew[3],memnew[4],memnew[5],memnew[6],memnew[7],''];
+    }
+    if (val == "MR"){
+      newoperand = this.state.memory[8];
+    }
+    this.setState({
+      currentOperand: newoperand,
+      memory: memnew
+    });
+  };
+
   render() {
     return (
       <div className="app">
-       <div className="calc-wrapper">
+        <div className="calc-wrapper">
           <Display display={(this.state.previousOperand+this.state.operation)}/>
           <Input input={this.state.currentOperand}/>
+          <div className="row">
+            <MemoryButton handleClick={this.memoryOperation}>MC</MemoryButton>
+            <MemoryButton handleClick={this.memoryOperation}>MR</MemoryButton>
+            <MemoryButton handleClick={this.memoryOperation}>M+</MemoryButton>
+            <MemoryButton handleClick={this.memoryOperation}>MS</MemoryButton>
+          </div>
           <div className="row">
             <ClearButton handleClear={() => this.setState({ 
               previousOperand: this.state.currentOperand,
@@ -99,6 +263,54 @@ class App extends Component {
           <div className="row">
             <Button handleClick={this.addToInput}>.</Button>
             <Button handleClick={this.addToInput}>0</Button>
+          </div>
+        </div>
+      
+        <div className="calc-wrapper">
+          <div className="row">
+            <Display display={this.state.memory[0]}/>
+            <MemoryButton handleClick={this.operationinMemory1}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory1}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[1]}/>
+            <MemoryButton handleClick={this.operationinMemory2}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory2}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[2]}/>
+            <MemoryButton handleClick={this.operationinMemory3}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory3}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[3]}/>
+            <MemoryButton handleClick={this.operationinMemory4}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory4}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[4]}/>
+            <MemoryButton handleClick={this.operationinMemory5}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory5}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[5]}/>
+            <MemoryButton handleClick={this.operationinMemory6}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory6}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[6]}/>
+            <MemoryButton handleClick={this.operationinMemory7}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory7}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[7]}/>
+            <MemoryButton handleClick={this.operationinMemory8}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory8}>MR</MemoryButton>
+          </div>
+          <div className="row">
+            <Display display={this.state.memory[8]}/>
+            <MemoryButton handleClick={this.operationinMemory9}>MC</MemoryButton>
+            <MemoryButton handleClick={this.operationinMemory9}>MR</MemoryButton>
           </div>
         </div>
       </div>
